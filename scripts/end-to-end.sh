c5bdcc6c6c1c7a6df794131a780e6a2ceac19293
Jakub Bednar <jakub.bednar@gmail.com>
2019-02-15 11:11:37 +0100

    #8: Added end-to-end test

--- scripts/end-to-end.sh
+++ scripts/end-to-end.sh
@@ -17,95 +17,34 @@
 
 #!/usr/bin/env bash
 
-set -e
-
-DEFAULT_INFLUXDB_VERSION="1.7"
-INFLUXDB_VERSION="${INFLUXDB_VERSION:-$DEFAULT_INFLUXDB_VERSION}"
-INFLUXDB_IMAGE=influxdb:${INFLUXDB_VERSION}-alpine
-
-DEFAULT_NIFI_VERSION="1.8.0"
-NIFI_VERSION="${NIFI_VERSION:-$DEFAULT_NIFI_VERSION}"
-NIFI_IMAGE=apache/nifi:${NIFI_VERSION}
-
-DEFAULT_TELEGRAF_VERSION="1.9"
-TELEGRAF_VERSION="${TELEGRAF_VERSION:-$DEFAULT_TELEGRAF_VERSION}"
-TELEGRAF_IMAGE=telegraf:${TELEGRAF_VERSION}
-
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-echo "Building nifi-influxdata-nar..."
-
-cd ${SCRIPT_PATH}/..
-
-mvn -B clean install -DskipTests
-
 echo
-echo "Stoping Docker containers..."
+echo "Querying data by 'select count(tweet_id) from tweets'"
 echo
 
-docker kill telegraf || true
-docker rm telegraf || true
+twitter=`(curl -s 'http://localhost:8086/query' --data-urlencode "db=twitter_demo" --data-urlencode "q=select count(tweet_id) from tweets")`
 
-docker kill nifi || true
-docker rm nifi || true
+echo "> result:" ${twitter}
 
-docker kill influxdb || true
-docker rm influxdb || true
-
-echo
-echo "Starting InfluxDB..."
-echo
-
-docker run \
-          --detach \
-          --name influxdb \
-          --publish 8086:8086 \
-          --publish 8089:8089/udp \
-          --volume ${SCRIPT_PATH}/../nifi-influx-database-services/src/test/resources/influxdb.conf:/etc/influxdb/influxdb.conf \
-      ${INFLUXDB_IMAGE}
-
-sleep 5
-
-#
-# Create database for Twitter demo
-#
-
-docker exec -ti influxdb sh -c "influx -execute 'create database twitter_demo'"
-docker exec -ti influxdb sh -c "influx -execute 'create database telegraf_nifi_demo'"
+echo ${twitter} | jq '.results[0].series[0].values[0][1] > 0' | grep -q 'true'
+if [[ $? -eq 0 ]]; then
+    echo "> check: success"
+else
+    echo "> check: fail..."  `(echo ${twitter} | jq '.results[0].series[0].values[0][1]')`
+    exit 1
+fi
 
 echo
-echo "Build Apache NiFi with demo..."
+echo "Querying data by 'select count(pid) from docker_container_status'"
 echo
 
-gzip < ${SCRIPT_PATH}/flow.xml > ${SCRIPT_PATH}/flow.xml.gz
-# docker cp nifi:/opt/nifi/nifi-current/conf/flow.xml.gz scripts/
-# gunzip -f scripts/flow.xml.gz
+docker_container_status=`(curl -s 'http://localhost:8086/query' --data-urlencode "db=telegraf_nifi_demo" --data-urlencode "q=select count(pid) from docker_container_status")`
 
-docker build -t nifi -f ${SCRIPT_PATH}/Dockerfile --build-arg NIFI_IMAGE=${NIFI_IMAGE} .
+echo "Query result: " ${docker_container_status}
 
-echo
-echo "Starting Apache NiFi..."
-echo
-
-docker run \
-    --detach \
-    --name nifi \
-    --publish 8080:8080 \
-	--publish 8007:8000 \
-	--publish 6666:6666 \
-	--link=influxdb \
-	nifi
-
-wget -S --spider --tries=20 --retry-connrefused --waitretry=5 http://localhost:8080/nifi/
-
-echo
-echo "Starting Telegraf..."
-echo
-
-docker run \
-    --detach \
-    --name=telegraf \
-    --net=container:nifi \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v ${SCRIPT_PATH}/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
-    ${TELEGRAF_IMAGE}
+echo ${docker_container_status} | jq '.results[0].series[0].values[0][1] > 0' | grep -q 'true'
+if [[ $? -eq 0 ]]; then
+    echo "> check: success"
+else
+    echo "> check: fail..."  `(echo ${docker_container_status} | jq '.results[0].series[0].values[0][1]')`
+    exit 1
+fi
