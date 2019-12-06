25df84c1cd8c96382357ae856395b3878df8b69a
Jakub Bednar <jakub.bednar@gmail.com>
2019-12-06 10:20:15 +0100

    fix: fix restarting InfluxDB

--- scripts/nifi-restart.sh
+++ scripts/nifi-restart.sh
@@ -81,7 +81,8 @@ docker run \
           --volume ${SCRIPT_PATH}/../nifi-influx-database-services/src/test/resources/influxdb.conf:/etc/influxdb/influxdb.conf \
       ${INFLUXDB_IMAGE}
 
-sleep 5
+echo "Wait to start InfluxDB"
+wget -S --spider --tries=20 --retry-connrefused --waitretry=5 http://localhost:8086/ping
 
 #
 # InfluxDB 2.0
@@ -97,7 +98,8 @@ docker run \
        --publish 9999:9999 \
        ${INFLUXDB_V2_IMAGE}
 
-sleep 5
+echo "Wait to start InfluxDB 2.0"
+wget -S --spider --tries=20 --retry-connrefused --waitretry=5 http://localhost:9999/metrics
 
 echo
 echo "Post onBoarding request, to setup initial user (my-user@my-password), org (my-org) and bucket (my-bucket)"
